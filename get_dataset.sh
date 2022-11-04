79de06307d9733dcf121fecd57aa2c7ae99a6b07
Tanveer J <77818526+mjan2021@users.noreply.github.com>
2022-11-04 17:06:05 -0400

    bash script

---
+++ get_dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+url='https://www.kaggle.com/datasets/kmader/skin-cancer-mnist-ham10000/download'
+file='HAM10k'
+
+mkdir $file
+cd $file
+
+echo "DOownloading"
+curl -L $url -o $file && unzip -q $file -d $dir && rm $file &
