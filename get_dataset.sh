10f8ee251024000a7f3965b1e432eb373bd12e80
Tanveer J <77818526+mjan2021@users.noreply.github.com>
2022-11-04 19:10:08 -0400

    train/test meta files

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 url='https://www.kaggle.com/datasets/kmader/skin-cancer-mnist-ham10000/download'
 file='HAM10k'
-
-mkdir $file
-cd $file
+dir='skincancer'
+mkdir $dir
+cd $dir
 
 echo "DOownloading"
-curl -L $url -o $file && unzip -q $file -d $file && rm $file
+curl -L $url -o $file && unzip -q $file -d $dir && rm $file
