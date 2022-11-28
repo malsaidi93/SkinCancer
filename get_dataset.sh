3898f231dc3c7d2eeafa5a11454f3db567fe4088
malsaidi93 <59228641+malsaidi93@users.noreply.github.com>
2022-11-27 20:42:04 -0500

    cleaned repository

--- get_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-url='https://www.kaggle.com/datasets/kmader/skin-cancer-mnist-ham10000/download'
-file='HAM10k'
-dir='skincancer'
-mkdir $dir
-cd $dir
-
-echo "DOownloading"
-curl -L $url -o $file && unzip -q $file -d $dir && rm $file
--- src/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "hello"
-sleep 3
