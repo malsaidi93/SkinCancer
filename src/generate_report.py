9e10bbe64db02bf8c9e222b82d4011429e79b44b
Tanveer J <mjan2021@fau.edu>
2024-08-26 15:20:21 -0400

    softmax

--- src/generate_report.py
+++ src/generate_report.py
@@ -5,21 +5,23 @@ import pandas as pd
 root = '../reports/'
 class_names = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc']
 
+import numpy as np
+from scipy.special import softmax
+
+
 def main():
     combined_reports = {}
     for file in os.listdir(root):
-        combined_reports[file] = {}
-        with open(root+file, 'r') as f:
-            report = json.load(f)
+        if file.endswith('.txt'):
+            combined_reports[file] = {}
+            with open(root+file, 'r') as f:
+                report = json.load(f)
             
-        for each_class in report.keys():
-            if each_class in class_names:
-                combined_reports[file][each_class] = report[each_class]['f1-score']
+            for each_class in report.keys():
+                if each_class in class_names:
+                    combined_reports[file][each_class] = report[each_class]['f1-score']
 
-            
     return combined_reports
-            
-            
 
 
 if __name__ == "__main__":
@@ -27,10 +29,24 @@ if __name__ == "__main__":
     pd.DataFrame(combined_reports).to_excel('../reports/combined_reports.xlsx')
     headers = ["Filename", "akiec", "bcc", "bkl", "df", "mel", "nv", "vasc"]
     table = []
+    forSoftmax = []
+    aug = []
     for filename, values in combined_reports.items():
         row = [filename]
         row.extend(values.values())
         table.append(row)
-        
+        aug.append(filename)
+        forSoftmax.append(list(values.values()))
+
     print(tabulate(table, headers=headers, tablefmt="pretty"))
     
+    # Tabulate & Apply Softmax to F1-Scores
+    softmax_data = softmax(np.array(forSoftmax), axis=1)
+    
+    tableSoftmax = []
+    for idx in range(0, len(forSoftmax)):
+        row = [aug[idx]]
+        row.extend(softmax_data[idx])
+        tableSoftmax.append(row)
+
+    print(tabulate(tableSoftmax, headers=headers, tablefmt="pretty"))
