68ae21f7a3f9d0e248c097d23158d4bd33f087bb
Tanveer J <77818526+mjan2021@users.noreply.github.com>
2024-11-13 21:46:48 -0500

    CNN2D class added to the model

--- src/models.py
+++ src/models.py
@@ -1,9 +1,46 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+from torch.nn import functional as F
 
-class cnn():
-    pass
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN2DModel(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN2DModel, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 7, stride=2, padding=3)
+        self.conv2 = nn.Conv2d(64, 192, 3, padding=1)
+        self.conv3 = nn.Conv2d(192, 384, 3, padding=1)
+        self.conv4 = nn.Conv2d(384, 256, 3, padding=1)
+        self.conv5 = nn.Conv2d(256, 256, 3, padding=1)
+        self.fc1 = nn.Linear(256 * 6 * 6, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, num_classes)
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        x = F.local_response_norm(x)
+
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        x = F.local_response_norm(x)
+
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = self.fc3(x)
+        return x
 
 
 def efficientnet():
